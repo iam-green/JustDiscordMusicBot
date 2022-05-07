31c05f5421d3a0704f54b2a9abc810566e3386d8
Yun Do Hyun <yundohyun050121@gmail.com>
2022-05-07 11:50:33 +0900

    edit: voiceStateUpdate and more

--- src/events/voiceStateUpdate.ts
+++ src/events/voiceStateUpdate.ts
@@ -1,15 +1,18 @@
 import { music } from "../modules/music";
 import { Event } from "../types/event";
+import { client } from "../loaders/discord";
 
 export default new Event("voiceStateUpdate", async (oldState,newState) => {
-    const server = music[music.findIndex(e=>e.guild_id==oldState.guild.id)];
-    if(!server) return;
-    if(oldState.channelId == server.voice_id) {
-        if(!newState.channelId) {/* Unexpectly bot left voice channel */
-            server.queue = [];
-            server.player.stop();
-        } else if(oldState.channelId != newState.channelId) { /* Moved bot */
-            server.voice_id = newState.channelId;
+    if(oldState.id != client.user.id) { /* Detect Bot Voice Status Update */
+        const server = music[music.findIndex(e=>e.guild_id==oldState.guild.id)];
+        if(!server) return;
+        if(oldState.channelId == server.voice_id) {
+            if(!newState.channelId) {/* Unexpectly Bot Left Voice Channel */
+                server.queue = [];
+                server.player.stop();
+            } else if(oldState.channelId != newState.channelId) { /* Moved Bot By Other */
+                server.voice_id = newState.channelId;
+            }
         }
     }
 });
