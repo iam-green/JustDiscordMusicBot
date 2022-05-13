513f9be9348b0cdfc70085442c7e83315cbce94a
Yun Do Hyeon <yundohyun050121@gmail.com>
2022-05-13 10:42:51 +0900

    edit: voiceStateUpdate event

--- src/events/voiceStateUpdate.ts
+++ src/events/voiceStateUpdate.ts
@@ -3,16 +3,17 @@ import { Event } from "../types/event";
 import { client } from "../loaders/discord";
 
 export default new Event("voiceStateUpdate", async (oldState,newState) => {
-    if(oldState.id == client.user.id) { /* Detect Bot Voice Status Update */
-        const server = music[music.findIndex(e=>e.guild_id==oldState.guild.id)];
-        if(!server) return;
-        if(oldState.channelId == server.voice_id) {
-            if(!newState.channelId) {/* Unexpectly Bot Left Voice Channel */
-                server.queue = [];
-                server.player.stop();
-            } else if(oldState.channelId != newState.channelId) { /* Moved Bot By Other */
-                server.voice_id = newState.channelId;
-            }
+    
+    /* Music */
+    const server = music[music.findIndex(e=>e.voice_id==oldState.channelId)];
+    if(!server) return; /* Detect Voice Channel Exist */
+    if(oldState.id == client.user.id) { /* Bot Voice State Updated */
+        if(!newState.channelId) { /* Unexpectly Bot Left Voice Channel */
+            server.queue = [];
+            server.player.stop();
+        } else if(oldState.channelId && oldState.channelId != newState.channelId) { /* Moved Bot By Other */
+            server.voice_id = newState.channelId;
         }
     }
+    
 });
